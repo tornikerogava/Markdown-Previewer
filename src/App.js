120401ecee7706b67228ed5bc40711cca935475b
tornikerogava <tokorogava@gmail.com>
2021-06-17 21:55:37 +0400

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
 import './App.css';
+import { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import gfm from 'remark-gfm';
+import {  MarkdownPreview  } from "react-marked-markdown";
+
 
 export default function App() {
+  
+
+  const InitialText= `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+  
+  Heres some code, \`<div></div>\`, between 2 backticks.
+  
+  \`\`\`
+  // this is multi-line code:
+  
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+  
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+  
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+  `;
+  const [markdown, setMarkdown] = useState(InitialText);
+
   return (
-    <div className="App">
-      sda
+    <div className="markdown__container">
+      <textarea id="preview"xtarea value={markdown} onChange={(i) => setMarkdown(i.target.value)} id="editor" />
+
+      <div id="preview">
+        <MarkdownPreview  markedOptions={{
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+   }}  value={markdown} className="markdown__preview" />
+      </div>
     </div>
   );
 }
